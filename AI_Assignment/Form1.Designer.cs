f17096c1994e0fdb45a12883f9853e0e0d1a7f38
Yahia Sweid <yahia.sweid@gmail.com>
2015-05-02 09:02:32 +0800

    drawing lines and circles

---
+++ AI_Assignment/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnLine;
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Location = new System.Drawing.Point(12, 77);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(59, 57);
+            this.btnCircle.TabIndex = 0;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(12, 14);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(59, 57);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.Text = "Line";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(12, 140);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(59, 57);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 413);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.btnCircle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+
+    }
+}
+
---
+++ AI_Assignment/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI {
+    public partial class Form1 : Form {
+        private Graphics g;
+
+        struct Node {
+            public int id;
+            public Point location;
+
+            public Node (Point location, int id) {
+                this.id = id;
+                this.location = location;
+            }
+        }
+
+        private List<Node> nodes;
+
+        enum Tool { NONE, LINE, CIRCLE };
+        Tool currentTool = Tool.NONE;
+
+        private Point firstPoint, secondPoint;
+
+        private int nodesCounter = 0;
+        private int edgesCounter = 0;
+
+
+        public Form1 () {
+            InitializeComponent();
+            g = this.CreateGraphics();
+
+            nodes = new List<Node>();
+
+            firstPoint = new Point(0, 0);
+            secondPoint = new Point(0, 0);
+
+        }
+
+        private void Form1_MouseClick (object sender, MouseEventArgs e) {
+            if (currentTool == Tool.CIRCLE) {
+                g.DrawEllipse(new Pen(Color.Black), e.Location.X, e.Location.Y, 40, 40);
+                g.DrawString(nodesCounter.ToString(), new Font("Times New Roman", 12), new SolidBrush(Color.Black), new Point(e.Location.X + 12, e.Location.Y + 12));
+                nodes.Add(new Node(e.Location, nodesCounter));
+                nodesCounter++;
+            } else if (currentTool == Tool.LINE) {
+                if (firstPoint.X == 0 && firstPoint.Y == 0)
+                    firstPoint = new Point(e.Location.X, e.Location.Y);
+                else {
+                    secondPoint = new Point(e.Location.X, e.Location.Y);
+                    g.DrawLine(new Pen(Color.Black), firstPoint, secondPoint);
+                    firstPoint = new Point(0, 0);
+                    secondPoint = new Point(0, 0);
+                    edgesCounter++;
+                }
+            }
+        }
+
+        private void btnLine_Click (object sender, EventArgs e) {
+            currentTool = Tool.LINE;
+        }
+
+        private void btnCircle_Click (object sender, EventArgs e) {
+            currentTool = Tool.CIRCLE;
+        }
+
+        private void btnColor_Click (object sender, EventArgs e) {
+            for (int i = 0; i < nodes.Count; i++) {
+                g.FillEllipse(new SolidBrush(Color.Red), nodes[i].location.X, nodes[i].location.Y, 40, 40);
+                g.DrawString(i.ToString(), new Font("Times New Roman", 12), new SolidBrush(Color.Black), new Point(nodes[i].location.X + 12, nodes[i].location.Y + 12));
+            }
+        }
+
+
+    }
+}
